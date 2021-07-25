955951e6c1478d2f49ed981bda54f703bf647289
gab <gxsoft@gmail.com>
2021-07-25 20:13:27 -0300

    others, history, how to SSH, aliases for GIT

---
+++ others/aliases.sh
@@ -0,0 +1,28 @@
+# Some good standards, which are not used if the user
+# creates his/her own .bashrc/.bash_profile
+
+# --show-control-chars: help showing Korean or accented characters
+alias ls='ls -F --color=auto --show-control-chars'
+alias ll='ls -l'
+
+case "$TERM" in
+xterm*)
+	# The following programs are known to require a Win32 Console
+	# for interactive usage, therefore let's launch them through winpty
+	# when run inside `mintty`.
+	for name in node ipython php php5 psql python2.7
+	do
+		case "$(type -p "$name".exe 2>/dev/null)" in
+		''|/usr/bin/*) continue;;
+		esac
+		alias $name="winpty $name.exe"
+	done
+	;;
+esac
+
+# gab Aliases for me -------------------------------------
+#-- git show nice graph of Branches, "arbolito" by @Freddier
+alias gittree="git log --all --graph --decorate --oneline"
+alias platzie="cd /e/platzi"
+alias dir="ls -al"
+alias md="mkdir $1"
